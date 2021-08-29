a2d270b1fcafb8cf0b5653225063e85b39067553
Hassan <hassansmallah@gmail.com>
2021-08-29 09:41:14 +0300

    issue-1: https://github.com/Hassan-Mallah/TealZincFootlambert/issues/1

--- AMinimalAPI .py
+++ AMinimalAPI .py
@@ -6,13 +6,13 @@ app = Flask(__name__)
 api = Api(app)
 
 
-class HelooWorld(Resource):
+class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
 
 # connect a link to the class
-api.add_resource(HelooWorld, '/')
+api.add_resource(HelloWorld, '/')
 
 if __name__ == '__main__':
     app.run(debug=True)
