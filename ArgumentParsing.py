add25caed58d304d912efc8dfed8f2cd27671c0f
Hassan <hassansmallah@gmail.com>
2021-09-05 16:33:16 +0300

    delete, put, get, post + example

--- ArgumentParsing.py
+++ ArgumentParsing.py
@@ -1,4 +1,10 @@
 # to do list: DELETE, GET, PUT, POST
+# example
+# Todos list: curl http://127.0.0.1:5000/
+# PUT add or update a task: curl http://localhost:5000/todo1 -d "data=Remember the milk" -X PUT
+# show a task:  curl http://localhost:5000/todo1
+# delete a task: curl http://localhost:5000/todos/todo3 -X delete -v
+# POST add a new task: curl http://localhost:5000/todos -d "task=something new new" -X post -v
 
 from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource
@@ -32,6 +38,7 @@ class Todo(Resource):
         del TODOS[todo_id]
         return '', 204
 
+    # requires task id and text
     def put(self, todo_id):
         args = parser.parse_args()
         task = {'task': args['task']}
@@ -39,7 +46,23 @@ class Todo(Resource):
         return task, 201
 
 
+# TodoList
+# returns list of list of Tasks, POST
+class TodoList(Resource):
+    def get(self):
+        return TODOS
+
+    # requires only task text
+    def post(self):
+        args = parser.parse_args()
+        todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1  # id of next item
+        todo_id = 'todo%i' % todo_id
+        TODOS[todo_id] = {'task': args['task']}
+        return  TODOS[todo_id], 201
+
+
 api.add_resource(Todo, '/todos/<todo_id>')
+api.add_resource(TodoList, '/todos')
 
 if __name__ == '__main__':
     app.run(debug=True)
